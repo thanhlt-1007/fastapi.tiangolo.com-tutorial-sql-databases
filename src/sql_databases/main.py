b4e0dd2a79723234154394f1f320cd0b427cb32a
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2025-03-17 21:30:42 +0700

    API DELETE /heroes/{hero_id}

--- src/sql_databases/main.py
+++ src/sql_databases/main.py
@@ -53,3 +53,15 @@ async def read_heroes(
     if not hero:
         raise HTTPException(status_code=404, detail="Hero not found")
     return hero
+
+@app.delete("/heroes/{hero_id}")
+async def delete_hero(
+    hero_id: int,
+    session: SessionDep,
+):
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    session.delete(hero)
+    session.commit()
+    return {"message": "Hero deleted successfully"}
